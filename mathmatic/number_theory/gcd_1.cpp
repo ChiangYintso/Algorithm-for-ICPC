e172160ab88098a2c4d9ca378c95aae771d0bed8
jiang <392711804@qq.com>
2020-08-16 19:02:32 +0800

    add math

--- mathmatic/number_theory/gcd_1.cpp
+++ mathmatic/number_theory/gcd_1.cpp
@@ -1,18 +1,18 @@
 #include<cstdio>
-int gcd(int a, int b)
-{
-    return b == 0 ? a : gcd(b, a%b);
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
 }
-int main()
-{
+
+int main() {
     int n;
     scanf("%d", &n);
-    while(n--){
+    while (n--) {
         int a, b;
         scanf("%d %d", &a, &b);
-        int c = b*2;
-        while(1){
-            if(gcd(a, c) == b){
+        int c = b * 2;
+        while (1) {
+            if (gcd(a, c) == b) {
                 printf("%d\n", c);
                 break;
             }
